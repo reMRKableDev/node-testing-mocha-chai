9f514caff77f10a3cf2e5fb3ceecb27129d1f3d5
Malcolm Kente <malcolm.kente@remrkabledev.com>
2020-03-14 14:37:07 +0100

    modified api file + test

--- 6. API-Unit-Test/app.js
+++ 6. API-Unit-Test/app.js
@@ -1,13 +1,16 @@
-const request = require("request");
+const axios = require("axios");
 
 let todosUrl = "https://jsonplaceholder.typicode.com/todos";
 
-let apiCall = function (callback) {
-    request.get({
-        url: todosUrl
-    }, callback);
+let apiCall = async () => {
+  try {
+    const results = await axios.get(todosUrl);
+    return results.data;
+  } catch (error) {
+    throw new Error(`Something went wrong: ${error}`);
+  }
 };
 
 module.exports = {
-    apiCall
+  apiCall
 };
--- 6. API-Unit-Test/test/appTest.js
+++ 6. API-Unit-Test/test/appTest.js
@@ -4,20 +4,17 @@ const chai = require("chai");
 const assert = chai.assert;
 
 describe("API Unit Test", function() {
-  it("tests if API call does not return an empty value", function(done) {
-    app.apiCall(function(error, response, body) {
-      let bodyObj = JSON.parse(body);
-      assert.isNotEmpty(bodyObj);
-      assert.isNotNull(bodyObj);
-      done();
-    });
+  it("tests if API call does not return an empty value", async () => {
+    const apiCallTest = await app.apiCall();
+
+    assert.isNotEmpty(apiCallTest);
+    assert.isNotNull(apiCallTest);
   });
-  it("tests if API call returns an array with multiple elements", function(done) {
-    app.apiCall(function(error, response, body) {
-      let bodyObj = JSON.parse(body);
-      assert.isArray(bodyObj);
-      assert.isAbove(bodyObj.length, 1); // checks that the length of bodyObj is above 1.
-      done();
-    });
+
+  it("tests if API call returns an array with multiple elements", async () => {
+    const apiCallTest = await app.apiCall();
+
+    assert.isArray(apiCallTest);
+    assert.isAbove(apiCallTest.length, 1);
   });
 });
