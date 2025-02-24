b595a25f4d42dbffd510818f5ea0a924b849255e
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-02-24 16:22:39 +0100

    chore: simplify tests

--- 1. Simple-Unit-Test/test/test.js
+++ 1. Simple-Unit-Test/test/test.js
@@ -3,15 +3,13 @@ const assert = require("assert");
 // describe() is used for grouping test suites. Describe can be nested.
 // it() is a test case.
 
-describe("Name Unit Test", function() {
+describe("Name Unit Test", function () {
   let actualValue = "Peter";
 
-  it("tests if the actualValue is the same as the expectedValue", function() {
-    let expectedValue = "Peter";
-    assert(actualValue === expectedValue);
+  it("tests if the actualValue is the same as the expectedValue", function () {
+    assert(actualValue === "Peter");
   });
-  it("tests if the actualValue isn't similar to the expectedValue", function() {
-    let expectedValue = "peter";
-    assert(actualValue !== expectedValue);
+  it("tests if the actualValue isn't similar to the expectedValue", function () {
+    assert(actualValue !== "peter");
   });
 });
