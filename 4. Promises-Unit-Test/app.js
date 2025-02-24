a5952fcbcaa071d2bdd7636c23c174596e10caa4
Malcolm R. Kente <malcolm.kente@remrkabledev.com>
2025-02-24 16:41:12 +0100

    chore: improvements for maintainability and better understanding for promise-unit test

--- 4. Promises-Unit-Test/app.js
+++ 4. Promises-Unit-Test/app.js
@@ -1,32 +1,41 @@
-const Promise = require('promise');
+/**
+ * @module app
+ * @description Demonstration of Promise-based functions for testing
+ */
+const Promise = require("promise");
 
-let groceriesBought = true;
+// This could be a configuration or state variable in a real application
+const groceriesBought = true;
 
-let shoppingPromiseResolve = function () {
-    return new Promise(function (resolve, reject) {
-        if (groceriesBought) {
-            let friendAppreciation = 'Thanks alot for the help!';
-            resolve(friendAppreciation);
-        } else {
-            let friendDissapointment = new Error();
-            reject(friendDissapointment);
-        }
-    });
+/**
+ * Creates a promise that resolves when groceries are bought
+ * @returns {Promise<string>} A promise that resolves with a thank you message
+ */
+const shoppingPromiseResolve = () => {
+  return new Promise((resolve, reject) => {
+    if (groceriesBought) {
+      resolve("Thanks a lot for the help!"); // Fixed typo: "alot" -> "a lot"
+    } else {
+      reject(new Error("No groceries were bought"));
+    }
+  });
 };
 
-let shoppingPromiseReject = function () {
-    return new Promise(function (resolve, reject) {
-        if (!groceriesBought) {
-            let friendAppreciation = 'Thanks alot for the help!';
-            resolve(friendAppreciation);
-        } else {
-            let friendDissapointment = new Error();
-            reject(friendDissapointment);
-        }
-    });
+/**
+ * Creates a promise that resolves when groceries are NOT bought
+ * @returns {Promise<string>} A promise that resolves with a thank you message
+ */
+const shoppingPromiseReject = () => {
+  return new Promise((resolve, reject) => {
+    if (!groceriesBought) {
+      resolve("Thanks a lot for the help!");
+    } else {
+      reject(new Error("Groceries were bought unexpectedly"));
+    }
+  });
 };
 
 module.exports = {
-    shoppingPromiseResolve,
-    shoppingPromiseReject
+  shoppingPromiseResolve,
+  shoppingPromiseReject,
 };
--- 4. Promises-Unit-Test/test/appTest.js
+++ 4. Promises-Unit-Test/test/appTest.js
@@ -1,30 +1,30 @@
-const app = require('../app');
-
-// Assertion library Chai
-const chai = require('chai');
-const chaiAsPromised = require('chai-as-promised');
-
+const app = require("../app");
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised).should();
 
+describe("shoppingPromiseResolve()", () => {
+  it("should fulfill the promise", () => {
+    return app.shoppingPromiseResolve().should.be.fulfilled;
+  });
 
-describe('Promise resolve Unit Test', function () {
-    it('tests if Promise is fulfilled', function () {
-        return app.shoppingPromiseResolve().should.be.fulfilled;
-    });
-    it('tests if the return value of Promise is resolve', function () {
-        return app.shoppingPromiseResolve().should.eventually.equal('Thanks alot for the help!');
-
-    });
-
+  it("should resolve with the correct thank you message", () => {
+    return app
+      .shoppingPromiseResolve()
+      .should.eventually.equal("Thanks a lot for the help!");
+  });
 });
 
-describe('Promise reject Unit Test', function () {
-    it('tests if Promise is fulfilled', function () {
-        return app.shoppingPromiseReject().should.not.be.fulfilled;
-    });
-    it('tests if the return value of Promise is reject', function () {
-        return app.shoppingPromiseReject().should.be.rejected;
+describe("shoppingPromiseReject()", () => {
+  it("should reject the promise", () => {
+    return app.shoppingPromiseReject().should.be.rejected;
+  });
 
-    });
+  it("should not fulfill the promise", () => {
+    return app.shoppingPromiseReject().should.not.be.fulfilled;
+  });
 
+  it("should reject with an error instance", () => {
+    return app.shoppingPromiseReject().should.be.rejectedWith(Error);
+  });
 });
